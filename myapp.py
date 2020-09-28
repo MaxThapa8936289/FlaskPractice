1959202e253907ffe494716d1831178b049c36bf
MaxThapa8936289 <max.thapa.student@gmail.com>
2020-09-28 10:29:22 +0100

    Added a file upload webpage

--- myapp.py
+++ myapp.py
@@ -1,6 +1,7 @@
 import flask
 
 from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -46,6 +47,19 @@ def login():
 @app.route('/hello/<user>')
 def hello_name(user):
     return render_template('hello.html', name=user)
+
+
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file2():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
 
 
 if __name__ == '__main__':
