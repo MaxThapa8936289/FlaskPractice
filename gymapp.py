b887cdc11b7a3f68760ce1053fc2cd71fb218179
MaxThapa8936289 <max.thapa.student@gmail.com>
2020-09-30 12:03:22 +0100

    added login page and authentiaction check

--- gymapp.py
+++ gymapp.py
@@ -18,13 +18,42 @@ def create_account():
 
 @app.route('/account_complete',  methods=['POST'])
 def account_complete():
-    user = request.form["username"]
-    pw = request.form["password"]
+    user = request.form['username']
+    pw = request.form['password']
     email = request.form["email"]
     with open('users.txt', 'a') as f:
         f.write(user+','+pw+','+email)
         f.write('\n')
-    return render_template('account_complete.html', name=user)
+    return redirect(url_for('userpage', username=user))
+
+
+@app.route('/u/<username>')
+def userpage(username):
+    return render_template('userpage.html', name=username)
+
+@app.route('/login')
+@app.route('/login/<err>')
+def login(err='none'):
+    print(err, type(err))
+    return render_template('login.html', error=err)
+
+
+@app.route('/login/authentication', methods=['POST'])
+def login_auth():
+    user = request.form["username"]
+    pw = request.form["password"]
+    success = False
+    with open('users.txt', 'r') as f:
+        for line in f:
+            line = line.split(',')
+            if line[0] == user and line[1] == pw:
+                print("success")
+                success = True
+                break
+    if success:
+        return redirect(url_for('userpage', username=user))
+    else:
+        return redirect(url_for('login', err='failed'))
 
 
 if __name__ == '__main__':
