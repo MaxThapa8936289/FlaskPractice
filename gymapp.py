d70ee3bdb9fe99c9bedfe6224d6c443c60714d6d
MaxThapa8936289 <max.thapa.student@gmail.com>
2020-09-30 09:44:34 +0100

    Added account info saving

--- gymapp.py
+++ gymapp.py
@@ -16,9 +16,14 @@ def create_account():
     return render_template('account_form.html')
 
 
-@app.route('/account_complete')
+@app.route('/account_complete',  methods=['POST'])
 def account_complete():
-    user = request.args.get('username')
+    user = request.form["username"]
+    pw = request.form["password"]
+    email = request.form["email"]
+    with open('users.txt', 'a') as f:
+        f.write(user+','+pw+','+email)
+        f.write('\n')
     return render_template('account_complete.html', name=user)
 
 
